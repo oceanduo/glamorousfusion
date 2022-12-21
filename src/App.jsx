f5b9e7c926a93630422b1d3391500406b3b0cebf
oceanduo <oceanduo@gmail.com>
2022-12-21 12:48:43 +1000

    set up bu duoduo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,40 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+
+import React from "react";
+
+
+
+const router = createBrowserRouter([
+  
+      {
+        path: "/",
+        element: <span>home</span>,
+      },
+      {
+        path: "/products/:id",
+        element: <input value='product' />,
+      },
+      {
+        path: "/product/:id",
+        element: <input value= 'hsahh'/>,
+      },
+    
+]);
+
 const App = () => {
-  return <div>Hello world!</div>;
+  return (
+    <>
+       <RouterProvider router={router} />
+    </>
+ 
+ )
+  
+
 };
 
 export default App;
